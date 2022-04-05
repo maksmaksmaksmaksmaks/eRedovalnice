76ac6a3bb86e5e666614062395545d0777f0e5cc
maks <maks.marolt@gmail.com>
2022-04-05 13:34:57 +0200

    for loop

--- src/main/java/com/eRedovalnice/App.java
+++ src/main/java/com/eRedovalnice/App.java
@@ -2,6 +2,9 @@ package com.eRedovalnice;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello word!");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(i);
+        }
     }
 }
