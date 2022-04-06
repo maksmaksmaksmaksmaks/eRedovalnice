4ffd3268552405a97a862389b9674491a2675a07
JellyBean1234 <anej.sukic@gmail.com>
2022-04-06 11:31:37 +0200

    poskus

--- src/main/java/com/eRedovalnice/App.java
+++ src/main/java/com/eRedovalnice/App.java
@@ -3,7 +3,6 @@ package com.eRedovalnice;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
         //create for loop for numbers form 1 to 10
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
